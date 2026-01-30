c33b6ee925730aef19b9e06ccab9bcd0ad298d6c
swakthika <swakthikachandran21@gmail.com>
2026-01-30 15:04:50 +0530

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-
+import Tab from './Tab'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Tab/>
   </StrictMode>,
 )
